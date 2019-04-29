d661a2291f887739d8a537bf83f0f13d956fac56
bajrushivlora <bajrushivlora@gmail.com>
2019-04-29 18:15:40 -0400

    wtf

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,11 @@ document.getElementById("weeknd_album").addEventListener("mouseout", stopSong);
 
 function playSong() {
     document.getElementById("weeknd").play();
-    console.log(play);
 }
 
 function stopSong() {
     document.getElementById("weeknd").load();
 }
-
 
 
 document.getElementById("pnd2_album").addEventListener("mouseover", playSong);
